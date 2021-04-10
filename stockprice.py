c1499007bfcedd991d7ad19069edb69f7659b5d7
Akshay553 <akshaykulkarni.0606@gmail.com>
2021-04-10 23:39:03 +0530

    Extracting stock values

--- stockprice.py
+++ stockprice.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-html = requests.get('http://money.rediff.com/companies/3m-India-Ltd/17010019').content
+html = requests.get('http://money.rediff.com/companies/Tata-Motors-Ltd/10510008').content
 
 soup = BeautifulSoup(html, 'html.parser')
 quote = float(soup.find(id='ltpid').get_text())
+marketcap =  soup.find(id='MarketCap').get_text()
+print("cmp ="+str(quote))
+print("marketcap ="+marketcap)
 
-print(quote)
+pe = soup.find(id='div_rcard_more').get_text()
+pe = pe.split('EPS')[0]
+print(pe)
