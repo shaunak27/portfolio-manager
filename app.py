7f6d2c0d47593e3acc438063322549e1bef06900
Shaunak Halbe <shaunak.halbe@gmail.com>
2021-04-17 15:43:47 +0530

    Fetch: Latest parameters

--- app.py
+++ app.py
@@ -278,6 +278,8 @@ def watchlist():
     for rec in records:
         d = companydetails(rec.stockname.upper())
         rec.cmp = d.get('cmp',0.0)
+        marketcap= d.get('marketcap',0.0),
+        peratio = d.get('pe',0.0),
         db.session.add(rec)
         db.session.commit()
     records = Watchlist.query.filter_by(
